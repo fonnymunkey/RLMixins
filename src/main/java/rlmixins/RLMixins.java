0858b71cf5aa3e02f8aa922e12edbd6bd89e8ae5
fonnymunkey <fonnymunkey@gmail.com>
2022-11-24 19:55:03 -0500

    1.0.1
    Add cancelling InF's multipart handling as RLCombat already handles it

--- src/main/java/rlmixins/RLMixins.java
+++ src/main/java/rlmixins/RLMixins.java
@@ -13,7 +13,7 @@ import rlmixins.handlers.ModRegistry;
 public class RLMixins
 {
     public static final String MODID = "rlmixins";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.1";
     public static final String NAME = "RLMixins";
 
     public static final Logger LOGGER = LogManager.getLogger();
