56a3acbc2a51821c3870a51d6a561a8e04489c61
fonnymunkey <fonnymunkey@gmail.com>
2023-03-31 12:36:50 -0400

    1.2.7
    Fix nether wart stew desync

--- src/main/java/rlmixins/mixin/foodexpansion/ItemNetherWartSoupMixin.java
+++ src/main/java/rlmixins/mixin/foodexpansion/ItemNetherWartSoupMixin.java
@@ -4,9 +4,14 @@ import lellson.foodexpansion.items.ItemFoodBasic;
 import lellson.foodexpansion.items.ItemNetherWartSoup;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.ItemStack;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 @Mixin(ItemNetherWartSoup.class)
 public abstract class ItemNetherWartSoupMixin extends ItemFoodBasic {
@@ -21,8 +26,18 @@ public abstract class ItemNetherWartSoupMixin extends ItemFoodBasic {
      */
     @Overwrite
     public ItemStack onItemUseFinish(ItemStack item, World world, EntityLivingBase player) {
+        if(!world.isRemote) {
+            Collection<PotionEffect> activeeffects = player.getActivePotionEffects();
+            ArrayList<PotionEffect> effectstoremove = new ArrayList<PotionEffect>();
+            for(PotionEffect effect : activeeffects) {
+                if(effect.getPotion().isBadEffect()) effectstoremove.add(effect);
+            }
+            for(PotionEffect potionEffect : effectstoremove) {
+                Potion potion = potionEffect.getPotion();
+                player.removePotionEffect(potion);
+            }
+        }
         player.setFire(5);
-        player.getActivePotionEffects().removeIf(potionEffect -> potionEffect.getPotion().isBadEffect());
         return super.onItemUseFinish(item, world, player);
     }
 }
