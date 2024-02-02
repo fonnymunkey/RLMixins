0c972d3e16689465c1256314175d5e85c94c2798
fonnymunkey <fonnymunkey@gmail.com>
2024-02-02 03:15:53 -0500

    Bump version 1.3.1

--- src/main/java/rlmixins/RLMixins.java
+++ src/main/java/rlmixins/RLMixins.java
@@ -34,7 +34,7 @@ import rlmixins.handlers.ModRegistry;
 public class RLMixins
 {
     public static final String MODID = "rlmixins";
-    public static final String VERSION = "1.3.0";
+    public static final String VERSION = "1.3.1";
     public static final String NAME = "RLMixins";
     public static final Logger LOGGER = LogManager.getLogger();
 
