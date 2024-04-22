7183eb2f41ca195d24d4bd5581ebb4adfa6f8b63
fonnymunkey <fonnymunkey@gmail.com>
2024-04-22 13:35:40 -0400

    Fix quark armor rune color patch defaulting to white

--- src/main/java/rlmixins/mixin/vanilla/LayerArmorBaseMixin.java
+++ src/main/java/rlmixins/mixin/vanilla/LayerArmorBaseMixin.java
@@ -1,5 +1,6 @@
 package rlmixins.mixin.vanilla;
 
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.entity.layers.LayerArmorBase;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -14,6 +15,7 @@ public abstract class LayerArmorBaseMixin {
             at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/GlStateManager;color(FFFF)V", ordinal = 1)
     )
     private static void rlmixins_vanillaLayerArmorBase_renderEnchantedGlint(float colorRed, float colorGreen, float colorBlue, float colorAlpha) {
+        GlStateManager.color(colorRed, colorGreen, colorBlue, colorAlpha);
         ASMHooks.applyRuneColor();
     }
 }
