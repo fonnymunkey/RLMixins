d1f481a734700dd959fa935f1e0025728ba1c1ed
Nischhelm <nischhelm@gmail.com>
2024-11-19 13:42:35 +0100

    cleaned unused imports

--- src/main/java/rlmixins/mixin/quark/UsageTickerMixin.java
+++ src/main/java/rlmixins/mixin/quark/UsageTickerMixin.java
@@ -1,12 +1,10 @@
 package rlmixins.mixin.quark;
 
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import vazkii.quark.client.feature.UsageTicker;
 
