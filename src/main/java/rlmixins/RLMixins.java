d0e741e4a24ed2db2fbc74076817ebece7a258f5
fonnymunkey <fonnymunkey@gmail.com>
2025-07-10 20:48:18 -0400

    1.4.1
    Fix missing sound error log

--- src/main/java/rlmixins/RLMixins.java
+++ src/main/java/rlmixins/RLMixins.java
@@ -16,7 +16,7 @@ import rlmixins.proxy.CommonProxy;
 public class RLMixins {
     
     public static final String MODID = "rlmixins";
-    public static final String VERSION = "1.4.0";
+    public static final String VERSION = "1.4.1";
     public static final String NAME = "RLMixins";
     public static final Logger LOGGER = LogManager.getLogger();
 
