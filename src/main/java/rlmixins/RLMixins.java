952dec55e81e1ba8a6f6f7561e4579f3abd351d1
fonnymunkey <fonnymunkey@gmail.com>
2025-07-19 05:46:52 -0400

    1.4.3
    Add player/mob stumbling movement effects based on config effect list
    Fix crash with Pirate Hat patch by cdstk
    Add enchantable Lycanite equipment handling by cdstk
    Bump version

--- src/main/java/rlmixins/RLMixins.java
+++ src/main/java/rlmixins/RLMixins.java
@@ -16,7 +16,7 @@ import rlmixins.proxy.CommonProxy;
 public class RLMixins {
     
     public static final String MODID = "rlmixins";
-    public static final String VERSION = "1.4.2";
+    public static final String VERSION = "1.4.3";
     public static final String NAME = "RLMixins";
     public static final Logger LOGGER = LogManager.getLogger();
 
